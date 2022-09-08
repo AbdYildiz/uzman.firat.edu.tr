71a0681dc0d76028981f634ce0993d23fb14ee81
AbdYildiz <abdullah-yildiz61@hotmail.com>
2022-09-08 16:10:34 +0300

    search added

--- src/MainPage.java
+++ src/MainPage.java
@@ -1,5 +1,118 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+import java.util.List;
+
+public class MainPage {
+    public WebDriver driver = new ChromeDriver();
+    public SoftAssert soft = new SoftAssert();
+    public JavascriptExecutor js = (JavascriptExecutor) driver;
+    public Actions act = new Actions(driver);
+
+    @BeforeSuite public void beforeSuite(){
+        driver.get("https://uzman.firat.edu.tr/");
     }
+
+    @BeforeMethod public void beforeMethod(){
+        driver.navigate().to("https://uzman.firat.edu.tr/");
+    }
+
+    @Test public void emptySearchBox(){
+        WebElement ele = driver.findElement(By.id("search"));
+        ele.sendKeys("",Keys.ENTER);
+        String message = (String)js.executeScript("return arguments[0].validationMessage;", ele);
+        Assert.assertEquals(message,"Please fill out this field.");
+    }
+
+    @Test public void emptyFieldSearch(){
+        act.moveToElement(driver.findElement(By.cssSelector(".input-select-click"))).perform();
+        driver.findElement(By.id("clearSelectAll")).click();
+
+        List<WebElement> fields = driver.findElements(By.cssSelector(".input-select input"));
+        for (WebElement e: fields) {
+            boolean f = e.isSelected();
+            soft.assertFalse(f);
+        }
+        soft.assertAll();
+
+        WebElement ele = driver.findElement(By.id("search"));
+        ele.sendKeys("mustafa",Keys.ENTER);
+        String message = (String)js.executeScript("return arguments[0].validationMessage;", ele);
+        System.out.println(message);
+        Assert.assertEquals(message,"- indexes alanı gereklidir.");
+    }
+
+    @Test public void checkFields() throws InterruptedException {
+        act.moveToElement(driver.findElement(By.cssSelector(".input-select-click"))).perform();
+        List<WebElement> fields = driver.findElements(By.cssSelector(".input-select input"));
+
+        for (int i = 1; i < fields.size(); i++) {
+            fields.get(i).click();
+            boolean f = fields.get(i).isSelected();
+            soft.assertFalse(f);
+        }
+        Thread.sleep(500);
+        if (fields.get(0).isSelected() == false) {
+            for (int i = 1; i < fields.size(); i++) {
+                boolean f = fields.get(i).isSelected();
+                soft.assertFalse(f);
+            }
+        }
+        Thread.sleep(500);
+
+        if (fields.get(0).isSelected() == true) {
+            for (int i = 1; i < fields.size(); i++) {
+                boolean t = fields.get(i).isSelected();
+                soft.assertTrue(t);
+            }
+        }
+        Thread.sleep(500);
+
+        for (int i = 1; i < fields.size(); i++) {
+            fields.get(i).click();
+            boolean t = fields.get(i).isSelected();
+            soft.assertTrue(t);
+        }
+        Thread.sleep(500);
+        soft.assertAll();
+    }
+
+    @Test public void searchButton(){
+        String caracter = "mustafa";
+        driver.findElement(By.id("search")).sendKeys(caracter);
+        driver.findElement(By.cssSelector(".btn-search")).click();
+        String element = driver.findElement(By.xpath("//p[contains(text(),'mustafa')]")).getText();
+        Assert.assertTrue(element.contains(caracter));
+    }
+
+    @Test (dataProvider = "clickable",timeOut = 5000) public void links(WebElement el, String url){
+        el.click();
+        Assert.assertEquals(driver.getCurrentUrl(),url);
+        driver.navigate().back();
+    }
+
+    @DataProvider public Object[][] clickable(){
+        Object[][] click = new Object[3][2];
+        click[0][0] = driver.findElement(By.xpath("//a[contains(text(),'Hakkımızda')]"));
+        click[0][1] = "https://uzman.firat.edu.tr/about-us";
+
+        click[1][0] = driver.findElement(By.xpath("//a[contains(text(),'Fırat Üniversitesi')]"));
+        click[1][1] = "https://www.firat.edu.tr/tr";
+
+        click[2][0] = driver.findElement(By.xpath("//a[contains(text(),'Dijital Dönüşüm ve Yazılım Ofisi')]"));
+        click[2][1] = "http://ddyo.firat.edu.tr/tr";
+
+        return click;
+    }
+
+    @AfterSuite public void afterSuite() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
 }
--- src/Search.java
+++ src/Search.java
@@ -1,2 +1,77 @@
-package PACKAGE_NAME;public class Search {
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class Search {
+    public WebDriver driver = new ChromeDriver();
+    public Actions act = new Actions(driver);
+    public SoftAssert soft = new SoftAssert();
+    public JavascriptExecutor js = (JavascriptExecutor) driver;
+
+    @BeforeClass public void beforeClass(){
+        driver.get("https://uzman.firat.edu.tr/search-results?indexes%5B0%5D=on&indexes%5B1%5D=on&indexes%5B2%5D=on&indexes%5B3%5D=on&indexes%5B4%5D=on&indexes%5B5%5D=on&indexes%5B6%5D=on&indexes%5B7%5D=on&indexes%5B8%5D=on&indexes%5B9%5D=on&arama=mu");
+        driver.manage().window().maximize();
+    }
+
+    @Test (dataProvider = "getData") public void checkButtons(String tab) throws IOException, InterruptedException {
+        driver.switchTo().window(tab);
+
+        List<WebElement> toProfile = driver.findElements(By.cssSelector("td a"));
+        for (WebElement a: toProfile) {
+            String url = a.getAttribute("href");
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+            soft.assertTrue(conn.getResponseCode() < 400,a.getText() + "          BROKEN LINK");
+        }
+
+        List<WebElement> details = driver.findElements(By.cssSelector("td button"));
+        for (WebElement e: details){
+            e.click();
+            Thread.sleep(300);
+            soft.assertTrue(driver.findElement(By.cssSelector(".btn-close")).isEnabled());
+            driver.findElement(By.cssSelector(".btn.btn-secondary")).click();
+            Thread.sleep(300);
+            js.executeScript("javascript:window.scrollBy(0,50)");
+        }
+        soft.assertAll();
+    }
+
+    @DataProvider public Object[] getData() throws InterruptedException {
+        List<WebElement> results = driver.findElements(By.cssSelector(".thumbnail"));
+        for (int i = 0; i < results.size(); i++) {
+            act.keyDown(Keys.LEFT_CONTROL).perform();
+            results.get(i).click();
+            Thread.sleep(300);
+        }
+        driver.close();
+
+        Set<String> windowHandles = driver.getWindowHandles();
+        Iterator<String> it = windowHandles.iterator();
+
+        Object[] data = new Object[windowHandles.size()];
+        for (int i = 0; i < windowHandles.size(); i++) {
+            data[i] = it.next();
+        }
+        return data;
+    }
+
+
+    @AfterClass public void afterClass() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
 }
